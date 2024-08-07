c9f56f2de189cb8bcffd0a81844cabcea2d4a0d6
Jesari82 <jries@js-ries.de>
2024-08-07 15:04:26 +0200

    Correct spelling roster

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home';
-import RoasterPage from './pages/RoasterPage';
+import RosterPage from './pages/RosterPage.jsx';
 import Leaderboard from './pages/Leaderboard';
 import BattlePage from './pages/BattlePage.jsx';
 import './components/PokemonCard.jsx';
@@ -14,7 +14,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/roaster" element={<RoasterPage />} />
+        <Route path="/roster" element={<RosterPage />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/battle" element={<BattlePage />} />
         
---
+++ src/pages/RosterPage.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const RosterPage = () => {
+  return (
+    <div  className="text-3xl font-bold mb-4">
+      <h1>My Roster</h1>
+    </div>
+  );
+}
+
+export default RosterPage;
