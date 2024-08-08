6727b1f88d552a95a0fc72b0e88858d0919716c5
yongqi8899 <1243994415@qq.com>
2024-08-08 11:15:47 +0200

    add battle dialog

--- src/components/BattleDialog.jsx
+++ src/components/BattleDialog.jsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
-const BattleDialog = ({ score}) => {
-  const [userName, setUserName] = useState("")
-  const userScore = score.player1
-  const pcScore = score.player2
-  console.log("close", score, userName);
-  const handleSubmit =() => {
-    console.log("close", score, userName);
+const BattleDialog = ({ score }) => {
+  const [userName, setUserName] = useState("");
+  const userScore = score.player1;
+  const pcScore = score.player2;
+  const leaderboardData = {
+    username: userName,
+    score: userScore
   }
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/leaderboard", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(leaderboardData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch Leaderboard");
+      }
+    } catch (error) {
+      console.error("Error fetching Leaderboard data:", error);
+    }
+  };
   return (
     <dialog id="my_modal_1" className="modal">
       <div className="modal-box">
         <form>
           <fieldset>
-            <p className="text-lg font-bold mb-6">{userScore > pcScore ? `You got ${userScore} Points, You win!` : `Computer got ${pcScore} Points, Computer wins!`}</p>
+            <p className="text-lg font-bold mb-6">
+              {userScore > pcScore
+                ? `You got ${userScore} Points, You win!`
+                : `Computer got ${pcScore} Points, Computer wins!`}
+            </p>
 
             <label className="input input-bordered flex items-center gap-2">
               Username
@@ -20,13 +40,15 @@ const BattleDialog = ({ score}) => {
                 type="text"
                 className="grow"
                 value={userName}
-                onChange={(e)=>setUserName(e.target.value)}
+                onChange={(e) => setUserName(e.target.value)}
                 placeholder="Please give your Username"
               />
             </label>
           </fieldset>
           <div className="modal-action">
-              <button className="btn" type="submit" onClick={handleSubmit}>Submit</button>
+            <button className="btn" type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
         </form>
       </div>
